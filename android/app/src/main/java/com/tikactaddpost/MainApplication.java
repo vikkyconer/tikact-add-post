21b3dbb369b9273166cbfc5d43584523494f2dd7
Vikas Chandra <vikkycorner@gmail.com>
2020-07-25 15:54:25 +0530

    slider for filters

--- android/app/src/main/java/com/tikactaddpost/MainApplication.java
+++ android/app/src/main/java/com/tikactaddpost/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import org.capslock.RNDeviceBrightness.RNDeviceBrightness;
 import com.rnfs.RNFSPackage;
 import com.arthenica.reactnative.RNFFmpegPackage;
 import com.brentvatne.react.ReactVideoPackage;
--- src/screens/AddScreen/components/Filters.js
+++ src/screens/AddScreen/components/Filters.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 import { style } from "../styles";
 import ProfilePic from "./ProfilePic";
 import { RNCamera } from "react-native-camera";
+import DeviceBrightness from "react-native-device-brightness";
+import Slider from "@react-native-community/slider";
 const { View } = require("react-native");
 
 const Filters = (props) => {
   const [filters, setFilters] = useState([]);
-  // const [selectedFilter, setSelectedFilter] = useState(0);
+  const [filterBrightness, setFilterBrightness] = useState(1);
 
   useEffect(() => {
+    DeviceBrightness.setBrightnessLevel(filterBrightness);
     const normal = RNCamera.Constants.WhiteBalance.auto;
     const f1 = RNCamera.Constants.WhiteBalance.sunny;
     const f2 = RNCamera.Constants.WhiteBalance.cloudy;
@@ -45,13 +48,27 @@ const Filters = (props) => {
     <View
       style={{
         ...style.bottomContainer,
-        flexDirection: "row",
         justifyContent: "space-around",
         paddingTop: 10,
       }}
     >
       <View style={style.background} />
-      {renderFilters()}
+      <View>
+        <Slider
+          style={{ width: "100%", height: 40 }}
+          minimumValue={0}
+          maximumValue={1}
+          value={filterBrightness}
+          minimumTrackTintColor="#FFFFFF"
+          maximumTrackTintColor="#000000"
+          onValueChange={(value) => {
+            setFilterBrightness(value);
+          }}
+        />
+      </View>
+      <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+        {renderFilters()}
+      </View>
     </View>
   );
 };
