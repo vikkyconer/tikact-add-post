67d8c645fc3eca97cab98055e95101f8f0223d4d
Vikas Chandra <vikkycorner@gmail.com>
2020-07-28 05:14:43 +0530

    styling like tiktok

--- src/screens/AddScreen/components/SpeedBar.js
+++ src/screens/AddScreen/components/SpeedBar.js
@@ -4,13 +4,58 @@ import { speeds } from "../constants";
 import { style } from "../styles";
 
 const SpeedBar = () => {
-  const getMultipleOptions = (arr, unit, currentValue) => {
+  const getMultipleOptions = (arr, currentValue) => {
     return (
-      <View style={{ flexDirection: "row", width: "100%" }}>
+      <View
+        style={{
+          flexDirection: "row",
+          width: "100%",
+          marginHorizontal: 40,
+        }}
+      >
         {arr.map((data, key) => {
+          const isSelectedValue = key === currentValue;
+          const initialBox = key === 0;
+          const lastBox = key === arr.length - 1;
           return (
-            <View key={key} style={{ width: 10, backgroundColor: key%2 ? "grey" : 'white' }}>
-              <Text>{data}x</Text>
+            <View
+              key={key}
+              style={{
+                width: 68,
+                height: 40,
+                alignItems: "center",
+              }}
+            >
+              <View
+                style={{
+                  position: "absolute",
+                  width: "100%",
+                  height: "100%",
+                  backgroundColor: isSelectedValue ? "white" : "black",
+                  opacity: isSelectedValue ? 1 : 0.2,
+                  borderTopLeftRadius: isSelectedValue ? 5 : initialBox ? 5 : 0,
+                  borderTopRightRadius: isSelectedValue ? 5 : lastBox ? 5 : 0,
+                  borderBottomLeftRadius: isSelectedValue
+                    ? 5
+                    : initialBox
+                    ? 5
+                    : 0,
+                  borderBottomRightRadius: isSelectedValue
+                    ? 5
+                    : lastBox
+                    ? 5
+                    : 0,
+                }}
+              />
+              <Text
+                style={{
+                  color: key === currentValue ? "black" : "white",
+                  fontSize: 15,
+                  marginTop: 9,
+                }}
+              >
+                {data}x
+              </Text>
             </View>
           );
         })}
@@ -26,7 +71,7 @@ const SpeedBar = () => {
         marginBottom: 10,
       }}
     >
-      {getMultipleOptions(speeds, "x", 0)}
+      {getMultipleOptions(speeds, 2)}
     </View>
   );
 };
