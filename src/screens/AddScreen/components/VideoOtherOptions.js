486eb5fb541b3952e948f5efbf2cbf1ef62fc495
Vikas Chandra <vikkycorner@gmail.com>
2020-07-28 12:22:00 +0530

    add progess bar of video record

--- src/screens/AddScreen/components/VideoOtherOptions.js
+++ src/screens/AddScreen/components/VideoOtherOptions.js
@@ -2,37 +2,47 @@ import React from "react";
 import { View, Text } from "react-native";
 import { soundIcon } from "../constants";
 import VideoEditTools from "./VideoEditTools";
+import { ProgressBar, Colors } from "react-native-paper";
 
 const VideoOtherOptions = (props) => {
   return (
-    <View
-      style={{
-        margin: 10,
-        flexDirection: "row",
-        justifyContent: "space-between",
-      }}
-    >
-      {props.crossIcon}
+    <View>
+      <View style={{ margin: 10 }}>
+        <ProgressBar
+          progress={0}
+          color="red"
+          style={{ height: 8, borderRadius: 5 }}
+        />
+      </View>
       <View
         style={{
+          margin: 10,
           flexDirection: "row",
-          position: "absolute",
-          left: "45%",
-          top: 10,
+          justifyContent: "space-between",
         }}
       >
-        {soundIcon}
-        <Text style={{ color: "white" }}>Sounds</Text>
+        {props.crossIcon}
+        <View
+          style={{
+            flexDirection: "row",
+            position: "absolute",
+            left: "45%",
+            top: 10,
+          }}
+        >
+          {soundIcon}
+          <Text style={{ color: "white" }}>Sounds</Text>
+        </View>
+        <VideoEditTools
+          flashCamera={props.flashCamera}
+          flashIcon={props.flashIcon}
+          setBottomContainer={props.setBottomContainer}
+          cameraSide={props.cameraSide}
+          setCameraSide={props.setCameraSide}
+          setShowSpeedOptions={props.setShowSpeedOptions}
+          showSpeedOptions={props.showSpeedOptions}
+        />
       </View>
-      <VideoEditTools
-        flashCamera={props.flashCamera}
-        flashIcon={props.flashIcon}
-        setBottomContainer={props.setBottomContainer}
-        cameraSide={props.cameraSide}
-        setCameraSide={props.setCameraSide}
-        setShowSpeedOptions={props.setShowSpeedOptions}
-        showSpeedOptions={props.showSpeedOptions}
-      />
     </View>
   );
 };
