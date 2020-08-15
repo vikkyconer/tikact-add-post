e67c5da469c023d0e634c64d765fe1e7e69f2be0
Vikas Chandra <vikkycorner@gmail.com>
2020-08-15 17:07:38 +0530

    add delay offset

--- src/screens/AddScreen/components/CameraScreen.js
+++ src/screens/AddScreen/components/CameraScreen.js
@@ -262,7 +262,7 @@ const CameraScreen = (props) => {
 
       // const audioDelay = Platform.Version < 27 ? 3 : 0;
       await RNFFmpeg.execute(
-        `-ss ${lastVideo.startAudio} -t ${lastVideo.endAudio} -i '${lastVideo.uri}' -ss ${lastVideo.startAudio} -t ${lastVideo.endAudio} -i '${selectedSound.url}' -c copy -map 0:v:0 -map 1:a:0 -shortest -q 1 ${processedVideoFile}`
+        `-ss ${lastVideo.startAudio} -t ${lastVideo.endAudio} -i '${lastVideo.uri}' -ss ${lastVideo.startAudio} -t ${lastVideo.endAudio} -itsoffset ${audioDelay} -i '${selectedSound.url}' -c copy -map 0:v:0 -map 1:a:0 -shortest -q 1 ${processedVideoFile}`
       );
       // if(Platform.Version < 27) {
       //   await RNFFmpeg.execute(
@@ -278,9 +278,8 @@ const CameraScreen = (props) => {
       //   //   `-ss ${lastVideo.startAudio} -t ${lastVideo.endAudio} -i '${selectedSound.url}' ${audioVideoFile}`
       //   // );
 
-        
       // } else {
-        
+
       // }
     } else if (!selectedSound && lastVideo.currentSpeed !== 1) {
       setProcessedVideos([...processedVideos, processedVideoFile]);
